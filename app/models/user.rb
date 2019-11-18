713356d22b440651279795550a9ad9806d46e8a8
Maxime Froment <mfroment99@gmail.com>
2019-11-18 18:41:21 +0900

    Added Cloudinary/CarrierWave and uploaders to Users and Connections

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  mount_uploader :photo, PhotoUploader
 end
