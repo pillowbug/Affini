de86624914123cc4d398f2e75ac0f48bb6a81486
Gerard Cabarse <gerard.cabarse@gmail.com>
2019-11-18 18:44:38 +0900

    updated routes for review

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :connections do
-    resources :glances
+    resources :glances, only: [:create, :update, :destroy]
     end
   end
-  resources :checkins
-  resources :attendees, only: [:index, :show]
-  resources :tags, only: [:index, :show, :create, :update, :destroy]
+  resources :checkins do
+    resources :attendees, only: [:create, :destroy]
+  end
+  # resources :tags, only: [:index, :show, :create, :update, :destroy]
   resources :users do
-    resources :connection_tags
+    resources :connection_tags, only [:create, :destroy]
   end
 end
