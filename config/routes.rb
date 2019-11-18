f4a8ead98a84c2041c69a3c3d56e605fb743e2c0
Maxime Froment <mfroment99@gmail.com>
2019-11-19 08:17:56 +0900

    Routes nesting reviewed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :connections do
+    resources :glances, only: [:create, :update, :destroy], shallow: true
+    resources :connection_tags, only: [:create, :destroy], shallow: true
+  end
+  resources :checkins do
+    resources :attendees, only: [:create, :destroy], shallow: true
+  end
+  # resources :tags, only: [:index, :show, :create, :update, :destroy]
+  resources :users
 end
