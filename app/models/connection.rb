1dd3d0315df98987095f33916320514d9735ac4d
Maxime Froment <mfroment99@gmail.com>
2019-11-19 11:14:31 +0900

    base associations

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,5 +1,10 @@
 class Connection < ApplicationRecord
   belongs_to :user
+  has_many :attendees
+  has_many :checkins, through: :attendees
+  has_many :connection_tags
+  has_many :tags, through: :connection_tags
+  has_many :glances
 
   mount_uploader :photo, PhotoUploader
 end
