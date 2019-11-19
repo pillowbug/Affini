c088032ec25f414b6847a0e51652a4598cab6a88
Maxime Froment <mfroment99@gmail.com>
2019-11-19 16:13:03 +0900

    Pundit policy setup

---
+++ app/policies/checkin_policy.rb
@@ -0,0 +1,19 @@
+class CheckinPolicy < ApplicationPolicy
+  def create?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.where(user: user)
+    end
+  end
+end
---
+++ app/policies/connection_policy.rb
@@ -0,0 +1,19 @@
+class ConnectionPolicy < ApplicationPolicy
+  def create?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.where(user: user)
+    end
+  end
+end
