69b6b3fb81b62503b67170872134a5b8b7668603
Maxime Froment <mfroment99@gmail.com>
2019-11-19 13:53:52 +0900

    Model validations

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -4,5 +4,17 @@ class Connection < ApplicationRecord
   has_and_belongs_to_many :checkins
   has_and_belongs_to_many :tags
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, allow_blank: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
   mount_uploader :photo, PhotoUploader
+
+  before_save :data_cleanup
+
+  private
+
+  def data_cleanup
+    self.email = email.strip if email
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,13 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :connections
+
+  validates :value, presence: true, uniqueness: true
+
+  before_save :data_cleanup
+
+  private
+
+  def data_cleanup
+    self.value = value.strip.gsub(/\s+/, ' ').titleize if value
+  end
 end
