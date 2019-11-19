3b45ff17c3b785bf33acf55b50862caa3f784dcc
Jiyoung Ko <jclareko@gmail.com>
2019-11-19 18:25:36 +0900

    WIP seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+def completed?(checkin)
+  if checkin[:time].to_f > Time.now.to_f
+    return false
+  else
+    return [true, false].sample
+  end
+end
+
 puts "Destroying Existing DB"
 User.destroy_all
 Connection.destroy_all
@@ -18,15 +22,17 @@ User.create!({
     email: "dontillman@org",
     password: 'secret',
 })
-puts " Added #{User.last.first_name}"
+puts "Added #{User.last.first_name}"
 
 #Three connections
 
 puts "Seeding Connections"
+
 Connection.create!({
   first_name: "Tom",
   last_name: "Niblo",
   frequency: "once a week",
+  birthday: Faker::Date.in_date_period.strftime("%d %B"),
   description: "le wagon",
   user_id: User.last.id
 })
@@ -34,6 +40,7 @@ Connection.create!({
   first_name: "Gerard",
   last_name: "Cabarse",
   frequency: "once a month",
+  birthday: Faker::Date.in_date_period.strftime("%d %B"),
   description: "le wagon",
   user_id: User.last.id
 })
@@ -41,6 +48,7 @@ Connection.create!({
   first_name: "Jiyoung",
   last_name: "Ko",
   frequency: "once a year",
+  birthday: Faker::Date.in_date_period.strftime("%d %B"),
   description: "le wagon",
   user_id: User.last.id
 })
@@ -48,19 +56,27 @@ Connection.create!({
   first_name: "Maxime",
   last_name: "Froment",
   frequency: "once a week",
+  birthday: Faker::Date.in_date_period.strftime("%d %B"),
   description: "le wagon",
   user_id: User.last.id
 })
-puts " Added #{Connection.all.count} connections"
+puts "Added #{Connection.all.count} connections"
 
 #Checkin
 
 puts "Seeding Checkin"
-  Checkin.create!({
+
+10.times do
+  args = {
     user_id: User.last.id,
-    rating: '1',
-    time: Time.now,
-    description: 'le wagon le wagon',
-    completed: true
-  })
-puts " Added first checkin"
+    rating: [1..5].sample,
+    time: Faker::Time.between_dates(from: Date.today - 5, to: Date.today + 5, format: :default),
+    description: 'le wagon'
+  }
+  args.merge!({completed: completed?(args)})
+  checkin = Checkin.new(args)
+  checkin.connections << Connection.all.sample(rand(1..2))
+  checkin.save
+end
+puts "Added #{Checkin.count} checkin"
+
