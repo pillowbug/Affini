f3ad8945e362bbc9765edb908ee5f45c7e937236
Maxime Froment <mfroment99@gmail.com>
2019-11-19 17:02:18 +0900

    regenerate schema

--- db/schema.rb
+++ db/schema.rb
@@ -53,13 +53,6 @@ ActiveRecord::Schema.define(version: 2019_11_19_050545) do
     t.index ["user_id"], name: "index_connections_on_user_id"
   end
 
-  create_table "connections_checkins", force: :cascade do |t|
-    t.bigint "connection_id"
-    t.bigint "checkin_id"
-    t.index ["checkin_id"], name: "index_connections_checkins_on_checkin_id"
-    t.index ["connection_id"], name: "index_connections_checkins_on_connection_id"
-  end
-
   create_table "connections_tags", force: :cascade do |t|
     t.bigint "connection_id"
     t.bigint "tag_id"
@@ -102,8 +95,6 @@ ActiveRecord::Schema.define(version: 2019_11_19_050545) do
   add_foreign_key "checkins_connections", "checkins"
   add_foreign_key "checkins_connections", "connections"
   add_foreign_key "connections", "users"
-  add_foreign_key "connections_checkins", "checkins"
-  add_foreign_key "connections_checkins", "connections"
   add_foreign_key "connections_tags", "connections"
   add_foreign_key "connections_tags", "tags"
   add_foreign_key "glances", "connections"
