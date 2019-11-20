918ddb5570a1df02c12cc59a4541d6c3794f6a80
Maxime Froment <mfroment99@gmail.com>
2019-11-20 14:17:39 +0900

    Add next checkin due date computation

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -26,6 +26,15 @@ class Connection < ApplicationRecord
     checkins.order(time: :desc).first
   end
 
+  def checkin_deadline
+    # returns date by which next checkin shall be scheduled
+    # nil if no target frequency, creation time stamp
+    return nil if frequency.nil?
+
+    last_time = last_checkin&.time || created_at
+    last_time.in frequency
+  end
+
   private
 
   def data_cleanup
