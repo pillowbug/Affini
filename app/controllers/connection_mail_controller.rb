57f892c798295cd18a20c4d98684e82f805ee8d9
Gerard Cabarse <gerard.cabarse@gmail.com>
2019-11-25 17:26:24 +0900

    sending an email works and creates a check-in simultaneously

--- app/controllers/connection_mail_controller.rb
+++ app/controllers/connection_mail_controller.rb
@@ -1,11 +1,32 @@
 class ConnectionMailController < ApplicationController
+  before_action :set_connection
+
   def send_connection_email
-    @connection = Connection.find(params[:connection_id])
     authorize @connection
-    ConnectionMailMailer.with(user_id: current_user.id,
-                              connection_id: @connection.id,
-                              subject: params[:email][:subject],
-                              body: params[:email][:body]).hello.deliver_now
+    ConnectionMailMailer.with(
+      user_id: current_user.id,
+      connection_id: @connection.id,
+      subject: params[:email][:subject],
+      body: params[:email][:body]
+    ).hello.deliver_now
+    create_email_checkin
     redirect_to connection_path(@connection)
+  end
+
+  def create_email_checkin
+    @checkin = Checkin.new(
+      user_id: current_user.id,
+      time: Time.now,
+      description: params[:email][:subject]
+    )
+    @checkin.user = current_user
+    @checkin.connections << @connection
+    @checkin.save
+  end
+
+  private
+
+  def set_connection
+    @connection = Connection.find(params[:connection_id])
   end
 end
