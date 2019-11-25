1cbaab22d257f087b369cf3823f65e67910a7409
Maxime Froment <mfroment99@gmail.com>
2019-11-25 11:08:39 +0900

    Modified live migration to default to nil

--- db/migrate/20191125005020_add_live_to_connections.rb
+++ db/migrate/20191125005020_add_live_to_connections.rb
@@ -1,5 +1,5 @@
 class AddLiveToConnections < ActiveRecord::Migration[5.2]
   def change
-    add_column :connections, :live, :datetime, default: Time.now
+    add_column :connections, :live, :datetime, default: nil
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_005019) do
+ActiveRecord::Schema.define(version: 2019_11_25_005020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2019_11_25_005019) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
-    t.datetime "live", default: "2019-11-25 01:43:50"
+    t.datetime "live"
     t.index ["user_id"], name: "index_connections_on_user_id"
   end
 
