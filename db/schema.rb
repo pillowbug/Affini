ab198f5199bd02938f868b502ddff3557af7f795
Maxime Froment <mfroment99@gmail.com>
2019-11-25 10:41:11 +0900

    Add admin user

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2019_11_25_005019) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
-    t.datetime "live", default: "2019-11-24 15:20:04"
     t.index ["user_id"], name: "index_connections_on_user_id"
   end
 
