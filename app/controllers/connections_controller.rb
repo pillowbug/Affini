a8a70d56df3267977d6082f2ee1e88924bc9acf3
Maxime Froment <mfroment99@gmail.com>
2019-11-27 01:17:43 +0900

    AJAXify onboarding page. For now functionally equivalent.

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -105,7 +105,12 @@ class ConnectionsController < ApplicationController
         # all good
       end
     end
-    redirect_to onboard_connections_path
+    respond_to do |format|
+      format.html { redirect_to onboard_connections_path }
+      format.js do
+        @connections = policy_scope(Connection).pending.order(created_at: :desc)
+      end
+    end
   end
 
   private
