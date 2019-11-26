ba64c00a79f9250b27ae277d61750dbc277f97d2
Jiyoung Ko <jclareko@gmail.com>
2019-11-26 11:33:39 +0900

    separate datepickers for bday and checkins

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,5 +1,12 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
 
+
+flatpickr(".datetimepicker", {
+  enableTime: true,
+  time_24hr: true
+})
+
 flatpickr(".datepicker", {
 })
+
