2cb28c434a4a1ca47f6ddaba700b49685d2ce1f8
Jiyoung Ko <jclareko@gmail.com>
2019-11-27 16:47:09 +0900

    particles js setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,10 @@
 import "bootstrap";
+import "particles.js"
 
 import "../plugins/flatpickr";
 
 import "../plugins/set_timeout"
 
 import "../plugins/fullcalendar";
+
+import "../plugins/particles";
