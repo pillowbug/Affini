81024d6c1e471fdf5f0273320d78b5be9362378f
Maxime Froment <mfroment99@gmail.com>
2019-11-27 18:15:56 +0900

    Checkin: validates 1 connection min, add default time ; Checkin form: ensure 1 connection selected in dropdown

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,16 +1,23 @@
 class Checkin < ApplicationRecord
+  after_initialize :init
+
   belongs_to :user
   has_and_belongs_to_many :connections
 
   validates :description, presence: true
   validates :time, presence: true
   validates :completed, inclusion: { in: [true, false] }
+  validates :connections, presence: true
 
   scope :past, -> { where('time < ?', Time.now) }
   scope :future, -> { where.not('time < ?', Time.now) }
   scope :completed, -> { where(completed: true) }
   scope :incomplete, -> { where(completed: false) }
   scope :feedback, -> { past.incomplete }
+
+  def init
+    self.time ||= Time.now
+  end
 
   def past?
     time < Time.now
